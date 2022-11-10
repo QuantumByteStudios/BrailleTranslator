6c33599cc9a111753202bf2c2829dab67a3306bb
Harshit Raheja <quantumbytestudios@gmail.com>
2022-11-11 01:35:57 +0530

    Update

--- run.sh
+++ run.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+# NOTE : YOU CAN MANUALLY COMPILE AND RUN
+
+if ! [ -x "$(command -v java)" ]; then
+  echo 'Error: java is not installed.' >&2
+  exit 1
+fi
+if ! [ -x "$(command -v javac)" ]; then
+  echo 'Error: javac is not installed.' >&2
+  exit 1
+fi
 clear
 javac main.java
 java main
